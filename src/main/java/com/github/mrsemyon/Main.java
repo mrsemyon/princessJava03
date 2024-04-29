dd24816289e73829faed422d06ab3f13f7f85dd9
Morozov Semyon <sky_net@mail.ru>
2024-04-29 13:44:23 +0500

    1st exercise finished

--- src/main/java/com/github/mrsemyon/Main.java
+++ src/main/java/com/github/mrsemyon/Main.java
@@ -1,7 +1,25 @@
 package com.github.mrsemyon;
 
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("First exercise:");
+        System.out.println(isStringContainsSameLetterCount("ooxx", 'o','x'));
+        System.out.println(isStringContainsSameLetterCount("xooxx", 'o','x'));
+        System.out.println(isStringContainsSameLetterCount("ooxXm", 'o','x'));
+        System.out.println(isStringContainsSameLetterCount("zpzpzpp", 'o','x'));
+        System.out.println(isStringContainsSameLetterCount("zzoo", 'o','x'));
+    }
 
+    public static boolean isStringContainsSameLetterCount(String str, Character firstCh, Character secondCh){
+        str = str.toLowerCase();
+        long firstChCount = str.chars()
+                .filter(s -> s == firstCh)
+                .count();
+        long secondChCount = str.chars()
+                .filter(s -> s == secondCh)
+                .count();
+        return firstChCount == secondChCount;
     }
 }
